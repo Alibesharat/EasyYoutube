0066000cf50f8c6782f9107f718f6905d48bdc1c
Ali Besharati <1368.ali.b@gmail.com>
2020-03-06 21:50:04 +0330

    Reasle To Publish

--- VideoDownloder/VideoDownloder.Android/MainActivity.cs
+++ VideoDownloder/VideoDownloder.Android/MainActivity.cs
@@ -23,8 +23,8 @@ namespace VideoDownloder.Droid
             LoadApplication(new App());
         }
 
-
        
+
 
     }
 }
--- VideoDownloder/VideoDownloder/App.xaml.cs
+++ VideoDownloder/VideoDownloder/App.xaml.cs
@@ -1,10 +1,10 @@
-﻿using Plugin.Multilingual;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Plugin.Multilingual;
 using System.Globalization;
 using VideoDownloder.Services;
 using Xamarin.Forms;
-using Microsoft.AppCenter;
-using Microsoft.AppCenter.Analytics;
-using Microsoft.AppCenter.Crashes;
 
 namespace VideoDownloder
 {
@@ -22,6 +22,7 @@ namespace VideoDownloder
 
         protected override void OnStart()
         {
+         
             AppCenter.Start("android=015f5fdd-9033-487a-8043-6e34bed6e9b2;",
                   typeof(Analytics), typeof(Crashes));
         }
